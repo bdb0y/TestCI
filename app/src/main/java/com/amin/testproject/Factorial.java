f456aaf7ddbbf4250a09e5923f53c101ee8048b4
ali <bandar3eda021@gmail.com>
2020-06-30 19:28:16 +0430

    changes made

--- app/src/main/java/com/amin/testproject/Factorial.java
+++ app/src/main/java/com/amin/testproject/Factorial.java
@@ -3,7 +3,7 @@ package com.amin.testproject;
 public class Factorial {
 
     public static long factorial(long n) {
-        if (n == 0 || n == 1) return 0;
+        if (n == 0 || n == 1) return 1;
         return n * factorial(n - 1);
     }
 }
